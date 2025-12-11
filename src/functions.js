0b6d460fae77c1595f220401a78e7457f768907d
Quantumdrill <quantumdrill317@gmail.com>
2025-12-11 04:16:38 -0500

    threejsfinal

---
+++ src/functions.js
@@ -0,0 +1,106 @@
+import * as THREE from "three"
+import { CCDIKSolver } from "three/examples/jsm/Addons.js"
+
+export function getBoneIndex(skeleton,name){
+    let output
+    skeleton.bones.forEach((bone,i) => {
+        if (bone.name === name){
+            output = i
+        }
+    });
+    return output
+}
+
+export function getSkinnedMeshSkeleton(imported){
+    imported.children.forEach(child=>{  //get skeleton and skinnedMesh from imported object
+        if(child.isBone!==true){
+            imported.skinnedMesh=child
+            imported.skeleton=child.skeleton
+        }
+    }) 
+}
+
+export function create3JointIK(skinnedMesh,skeleton,[target,effector,midJoint,topJoint]){
+    const iks = [{
+        target: getBoneIndex(skeleton,target),
+        effector: getBoneIndex(skeleton,effector),
+        links: [
+            {index: getBoneIndex(skeleton,midJoint)},
+            {index: getBoneIndex(skeleton,topJoint)}
+        ],
+        iteration: 10,
+    }]
+    return new CCDIKSolver(skinnedMesh,iks)
+}
+
+export function orientConstraint(driver,child,parent){ //world orientation to local, aka orient constraint
+    let driver_WR
+    if (driver.isQuaternion || driver.isEuler){ // depends on if the upv is a vector or an object
+        driver_WR = driver
+    } else {
+        driver_WR = driver.getWorldQuaternion(new THREE.Quaternion())
+    }
+    const parent_WR = parent.getWorldQuaternion(new THREE.Quaternion())
+    child.quaternion.copy(driver_WR).premultiply(new THREE.Quaternion().copy(parent_WR).invert())
+    child.updateMatrix()
+}
+
+export function pointConstraint(world,child,parent){ //world position to local, aka point constraint
+    let worldPos
+    if (world.isVector3){ // depends on if the upv is a vector or an object
+        worldPos = world
+    } else {
+        worldPos = world.getWorldPosition(new THREE.Vector3())
+    }
+    const parentWMatrix = parent.matrixWorld
+    const parentiWMatrix = new THREE.Matrix4().copy(parentWMatrix).invert()
+    const localPos = new THREE.Vector3().copy(worldPos).applyMatrix4(parentiWMatrix)
+    child.position.copy(localPos)
+}
+
+export function aimConstraint(obj,target,upv,parent=new THREE.Vector3(0,0,0),targetIsLocal=true,frontAxis=new THREE.Vector3(0,0,1),upAxis=new THREE.Vector3(0,1,0)){
+    // the targetV .makeBasis() takes in is a local vector
+    // the frontAxis & upAxis is used to determine which direction of the object is self is considered the front and the side, default is z front, y up, just like how threejs defines
+    let upV = upv
+    let targetV
+    let frontA = frontAxis
+    let upA = upAxis
+    if (target.isVector3){ // depends on if the target is a vector or an object
+        targetV = target
+        if (!targetIsLocal){ // turns targetV a local vector if not
+            targetV.sub(obj.getWorldPosition(new THREE.Vector3)) 
+        }
+    } else {
+        targetV = target.getWorldPosition(new THREE.Vector3)
+        targetV.sub(obj.getWorldPosition(new THREE.Vector3)) 
+    }
+    upV.normalize()
+    targetV.normalize()
+    let sideV = new THREE.Vector3().crossVectors(upV,targetV).normalize()
+    frontA.normalize()
+    upA.normalize()
+    let sideA = new THREE.Vector3().crossVectors(upA,frontA).normalize()
+    //axis offset is the offset from the desired front & up basic to the default basic of z front and y up
+    let axisOffset = new THREE.Quaternion().setFromRotationMatrix(new THREE.Matrix4().makeBasis(sideA,upA,frontA)).invert()
+    let aimQuat = new THREE.Quaternion().setFromRotationMatrix(new THREE.Matrix4().makeBasis(sideV,upV,targetV)).multiply(axisOffset)
+    let aimQuat2 = new THREE.Quaternion().setFromRotationMatrix(new THREE.Matrix4().makeBasis(sideV,upV,targetV))
+    orientConstraint(aimQuat,obj,parent)
+}
+
+export function poleVectorConstraint(ik,eff,elbow,shoulder,pv,parent){
+    let effPos = eff.getWorldPosition(new THREE.Vector3())
+    let elbowPos = elbow.getWorldPosition(new THREE.Vector3())
+    let shoulderPos = shoulder.getWorldPosition(new THREE.Vector3())
+    let pvPos = pv.getWorldPosition(new THREE.Vector3())
+    let ikPos = ik.getWorldPosition(new THREE.Vector3())
+
+    let shoulderToPV = new THREE.Vector3().subVectors(pvPos,shoulderPos)
+    let shoulderToIK = new THREE.Vector3().subVectors(ikPos,shoulderPos)
+    let shoulderToElbow = new THREE.Vector3().subVectors(elbowPos,shoulderPos)
+
+    let angle = new THREE.Vector3().copy(new THREE.Vector3().subVectors(effPos,shoulderPos)).angleTo(new THREE.Vector3().subVectors(elbowPos,shoulderPos)) 
+    let normal = new THREE.Vector3().crossVectors(shoulderToIK,shoulderToPV).normalize()
+    let upv = new THREE.Vector3().crossVectors(shoulderToElbow,normal)
+    let shoulderAimVector = new THREE.Vector3().copy(shoulderToIK).multiplyScalar(Math.cos(angle)).add(new THREE.Vector3().crossVectors(normal,shoulderToIK).multiplyScalar(Math.sin(angle))) //Rodrigues formula
+    aimConstraint(shoulder,shoulderAimVector,upv,parent,true,new THREE.Vector3(-1,0,0),new THREE.Vector3(0,0,-1))
+}
---
+++ src/main.js
@@ -0,0 +1,190 @@
+import * as THREE from "three"
+import {basicMesh,standardMesh,phongMesh,lambertMesh,physicalMesh} from "./defaultMeshes.js"
+import {pointLight,directionalLight} from "./light.js"
+import Model from "./model.js"
+import { texture } from "three/tsl"
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+import { CCDIKSolver } from "three/examples/jsm/Addons.js"
+import {manager} from "./manager.js"
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { getBoneIndex, create3JointIK, orientConstraint, pointConstraint, aimConstraint, getSkinnedMeshSkeleton, poleVectorConstraint } from "./functions.js"
+import {gsap} from "gsap"
+
+const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer({antialias:true})
+const fov = 23
+const cam = new THREE.PerspectiveCamera(fov, window.innerWidth/window.innerHeight,0.1,1000)
+// const camControls = new OrbitControls( cam, renderer.domElement );
+cam.position.set(0,13,15)
+let clock = new THREE.Clock()
+const loadingManager = manager()
+const modelLoader = new FBXLoader(loadingManager);
+const char = {}
+const lights = {}
+const anims = {}
+const mixers = []
+
+function init(){
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    lights.key = directionalLight({x:-5,y:5,z:5})
+    scene.add(lights.key)
+
+    // char rig model load
+    modelLoader.load("/rig.fbx",(loaded)=>{
+        char.mesh = loaded
+        const charMat = new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            emissive: 0xffffff,
+            emissiveIntensity: 0.04,
+            flatShading: true,
+            specular: 0xffffff,
+            shininess: 5
+        })
+        getSkinnedMeshSkeleton(char.mesh)
+        char.mesh.skinnedMesh.material = charMat
+        const helper = new THREE.SkeletonHelper(char.mesh)
+        
+        scene.add(char.mesh)
+
+        char.IkSolver = create3JointIK(char.mesh.skinnedMesh,char.mesh.skeleton,["ikHandle","hand_R","elbow_R","shoulder_R"])
+        char.ikHandle = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"ikHandle")]
+        char.rootBone = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"root")]
+        char.shoulder = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"shoulder_R")]
+        char.elbow = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"elbow_R")]
+        char.hand = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"hand_R")]
+        char.shoulderParent = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"spine2")]
+        char.spine1 = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"spine1")]
+        char.hip = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"hip")]
+        char.hip.restPos = char.hip.getWorldPosition(new THREE.Vector3())
+        char.head = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"head")]
+        char.neck = char.mesh.skeleton.bones[getBoneIndex(char.mesh.skeleton,"neck")]
+
+    })
+
+    //run after fbx is loaded
+    loadingManager.onLoad = ()=>{
+        console.log("loaded")
+
+        char.pv = new THREE.Object3D()
+        char.pv.position.y = -15
+
+        //mouse interaction
+        anims.charToCamDist = cam.position.z
+        anims.charToPointerHoverDist = 1.3
+        anims.pointerClickDist = 1
+        anims.planeToCamDist = anims.charToCamDist - anims.charToPointerHoverDist - anims.pointerClickDist
+        anims.wristToFingerTipDist = 1.9
+        anims.camHeight = cam.position.y
+        anims.curserToPointerMultiplier = Math.tan(fov/2/180*Math.PI)*(anims.planeToCamDist)
+        anims.wristRefBaseZ = -2
+        anims.wristRefBase = new THREE.Vector3(-2,6,anims.wristRefBaseZ) // a point in the back of the character, the line between the fingertip and this is used to determine the orientation and the position of the wrist
+        anims.pointerHoverZ = anims.charToPointerHoverDist
+        anims.pointerZ = anims.pointerHoverZ
+        anims.pointerPos = new THREE.Vector3(0,0,anims.pointerHoverZ)
+        document.addEventListener("mousemove",e=>{
+            anims.x = (e.clientX/window.innerWidth*2-1)*(window.innerWidth/window.innerHeight) // get x position -1*ratio to 1*ratio
+            anims.y = -(e.clientY/window.innerHeight*2-1) // get y position: -1 to 1
+            anims.pointerY = anims.y*anims.curserToPointerMultiplier + anims.camHeight // the local space of the root bone is x up, z side
+            anims.pointerX = anims.x*anims.curserToPointerMultiplier
+            anims.pointerPos.setComponent(0,anims.pointerX)
+            anims.pointerPos.setComponent(1,anims.pointerY)
+
+            char.hip.rotation.y = (anims.x+0.2)*0.1
+            char.hip.position.x = char.hip.restPos.y+(Math.abs(anims.x+0.2)+anims.y)*0.7
+            char.spine1.rotation.y = (anims.x+0.2)*0.15
+            char.spine1.rotation.x = (anims.x+1)*0.2
+            char.pv.position.x = (anims.x-1)*2
+            char.pv.position.z = (anims.x+2)*3
+            char.pv.position.y = (anims.y+anims.x)*5
+            console.log(char.pv.position)
+            char.head.rotation.z = (anims.y)*0.3
+            char.head.rotation.x = (anims.x)*0.1
+
+            clickWristUpdate()
+        })
+        anims.tlDown = {isActive:()=>{return false}}
+        anims.tlUp = {isActive:()=>{return false}}
+        anims.clickDuration = 0.1
+        anims.releasable = false
+        document.addEventListener("mousedown",e=>{
+            anims.mousedown = true
+        })
+        document.addEventListener("mouseup",e=>{
+            anims.mousedown = false
+        })
+        anim()
+    }
+}
+
+function clickWristUpdate(){
+    anims.wristRefLine = new THREE.Vector3().subVectors(anims.wristRefBase,anims.pointerPos) // line from fingertip to the wristRefBase
+    anims.fingerTipToWristVec = new THREE.Vector3().copy(anims.wristRefLine).setLength(anims.wristToFingerTipDist)
+    anims.wristToFingerTipVec = new THREE.Vector3().copy(anims.fingerTipToWristVec).negate()
+    anims.wristCurrentZ = char.hand.getWorldPosition(new THREE.Vector3()).z
+    anims.newWristPos = new THREE.Vector3().addVectors(anims.pointerPos,anims.fingerTipToWristVec)
+    pointConstraint(anims.newWristPos,char.ikHandle,char.rootBone)
+    
+    anims.wristUpV = new THREE.Vector3().crossVectors(new THREE.Vector3(-1,0,0),anims.wristToFingerTipVec).normalize()
+    aimConstraint(char.hand,anims.wristToFingerTipVec,anims.wristUpV,char.elbow,true,new THREE.Vector3(-7,1,0.5),anims.wristUpV)
+}
+
+function clickAnim(){
+    if (!anims.tlUp.isActive()&&!anims.tlDown.isActive()&&anims.mousedown === true&&anims.releasable === false){ 
+        anims.releasable = true // releasable is used to make sure the anim is fired only once
+        anims.tlDown = gsap.timeline()
+        anims.tlDown.to(char.ikHandle.position,{
+            y: "-="+anims.pointerClickDist*0.7,
+            duration: anims.clickDuration
+        }).to(anims,{
+            wristRefBaseZ: "+="+anims.pointerClickDist,
+            duration: anims.clickDuration,
+        },"<").to(anims,{
+            pointerZ: "+="+anims.pointerClickDist,
+            duration: anims.clickDuration
+        },"<")
+        anims.tlDown.eventCallback("onUpdate", () => {
+            anims.wristRefBase.setComponent(2,anims.wristRefBaseZ)
+            anims.pointerPos.setComponent(2,anims.pointerZ)
+            clickWristUpdate()
+        })
+    }
+    if (!anims.tlUp.isActive()&&!anims.tlDown.isActive()&&anims.mousedown === false&&anims.releasable === true){
+        anims.releasable = false
+        anims.tlUp = gsap.timeline()
+        anims.tlUp.to(char.ikHandle.position,{
+            y: "+="+anims.pointerClickDist*0.7,
+            duration: anims.clickDuration
+        }).to(anims,{
+            wristRefBaseZ: "-="+anims.pointerClickDist,
+            duration: anims.clickDuration
+        },"<").to(anims,{
+            pointerZ: "-="+anims.pointerClickDist,
+            duration: anims.clickDuration
+        },"<")
+        anims.tlUp.eventCallback("onUpdate", () => {
+            anims.wristRefBase.setComponent(2,anims.wristRefBaseZ)
+            anims.pointerPos.setComponent(2,anims.pointerZ)
+            clickWristUpdate()
+        })
+    }
+}
+
+function anim(){
+    const delta = clock.getDelta()
+    for (const mixer of mixers){
+        mixer.update(delta)
+    }
+    
+    poleVectorConstraint(char.ikHandle,char.hand,char.elbow,char.shoulder,char.pv,char.shoulderParent,new THREE.Euler())
+    char.IkSolver.update()
+    clickAnim()
+
+    // camControls.update()
+
+    renderer.render(scene,cam)
+    requestAnimationFrame(anim)
+}
+
+init()
